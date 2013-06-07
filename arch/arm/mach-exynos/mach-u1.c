a80ec1c3ee1ccb0e5a4c779e49c18363bb897cab
sakindia123 <sa6915@gmail.com>
2013-06-07 10:42:30 +0530

    mach-u1: use smdk4412 temperature values

--- arch/arm/mach-exynos/mach-u1.c
+++ arch/arm/mach-exynos/mach-u1.c
@@ -7432,8 +7432,8 @@ static struct platform_device *smdkc210_devices[] __initdata = {
 /* below temperature base on the celcius degree */
 struct s5p_platform_tmu u1_tmu_data __initdata = {
 	.ts = {
-		.stop_1st_throttle  = 61,
-		.start_1st_throttle = 64,
+		.stop_1st_throttle  = 78,
+		.start_1st_throttle = 80,
 		.stop_2nd_throttle  = 87,
 		.start_2nd_throttle = 103,
 		.start_tripping     = 110,
